2f1de82da7029319cb11c80c0fa2ab8b904be0aa
utente <utente@PC-di-Fed.station>
2021-01-14 20:45:43 +0100

    commit della sottrazione

---
+++ calcolatriceg2/src/main/java/Sottrazione.java
@@ -0,0 +1,15 @@
+
+public class Sottrazione {
+    private double numero,numero1;
+
+    public Sottrazione(double numero, double numero1) {
+        this.numero = numero;
+        this.numero1 = numero1;
+    }
+    
+    public static double calcolaSottrazione(double numero,double numero1){
+        double risultato;
+        risultato=numero-numero1;
+        return risultato;
+    }
+}
