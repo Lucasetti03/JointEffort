35f2594e253e13153d52bfc5ad87ee994aef2095
utente <utente@PC-di-Fed.station>
2021-01-14 20:52:30 +0100

    commit del main con sottrazione

---
+++ calcolatriceg2/src/main/java/Gestore.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+public class Gestore {
+    public static void main(String[] args){
+       int operazione;
+      double numero, numero1;
+Scanner scan = new Scanner(System.in);
+System.out.println("Scegliere il tipo di operazione:");
+System.out.print("Inserire 1 per l'addizione, 2 per la sottrazione, 3 per la moltiplicazione, 4 per la divisione, 0 per uscire: ");
+operazione = scan.nextInt();
+numero=scan.nextInt();
+numero1=scan.nextInt();
+switch (operazione) {
+    case 1:
+       Somma.calcolaSomma(numero,numero1);
+       break;
+       
+    case 2:
+       Sottrazione.calcolaSottrazione(numero, numero1);
+       break;
+    
+    case 3:
+        
+    break;
+    
+    case 4:
+        
+    break;
+    
+    default :
+    System.out.print("Uscita programma");
+   }
+  } 
+ }
+
